3a98311d03c8626482d46cfd5f5abba3ab859b0b
Audrey Carlsen <acarlsen@seattletimes.com>
2015-02-02 14:10:54 -0800

    Adds search field drop down

--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,23 @@
 var app = require("./application");
 
 app.controller("SchoolController", ["$scope", function($scope) {
+
+  var all = window.schoolData;
+
+  $scope.searchField = "name";
+
+  $scope.search = function() {
+    value = $scope.searchText.toLowerCase();
+    console.log(value)
+    if (!value) {
+      return $scope.found = [];
+    }
+    $scope.found = all.filter(function(item) {
+      return item[$scope.searchField].toLowerCase().indexOf(value) > -1;
+    });
+  };
+
+  $scope.found = [];
+
   $scope.allSchools = window.schoolData;
 }]);
